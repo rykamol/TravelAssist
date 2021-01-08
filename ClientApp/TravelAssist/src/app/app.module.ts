c9f1b3bca56b6dcc0043d02b4132087604b83b22
kamol roy <kamolroy.din@gmail.com>
2021-01-08 21:24:08 +0600

    added various components but yet to implement all

--- ClientApp/TravelAssist/src/app/app.module.ts
+++ ClientApp/TravelAssist/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NewSpotComponent } from './components/new-spot/new-spot.component';
+import { SpotListComponent } from './components/spot-list/spot-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent,
+    RegisterComponent,
+    NewSpotComponent,
+    SpotListComponent
   ],
   imports: [
     BrowserModule,
---
+++ ClientApp/TravelAssist/src/app/components/new-spot/new-spot.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NewSpotComponent } from './new-spot.component';
+
+describe('NewSpotComponent', () => {
+  let component: NewSpotComponent;
+  let fixture: ComponentFixture<NewSpotComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NewSpotComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NewSpotComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
