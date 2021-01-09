bd091f339950e1b615b587f0756435b1eb59ba68
kamol roy <kamolroy.din@gmail.com>
2021-01-09 23:33:00 +0600

    added two others components and setup the route of travel assist client app

--- ClientApp/TravelAssist/src/app/app-routing.module.ts
+++ ClientApp/TravelAssist/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
+import { NewSpotComponent } from './components/new-spot/new-spot.component';
+import { SpotListComponent } from './components/spot-list/spot-list.component';
 import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
+
+  {path:"",component:SpotListComponent},
+  {path:"new-spot",component:NewSpotComponent},
   {path:"register",component:RegisterComponent},
   {path:"login",component:LoginComponent}
 
